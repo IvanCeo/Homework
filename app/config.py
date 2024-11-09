e71f0c29053ceae52b2fea4a6964dd455e6effb0
Ivan <ceo@ivan-victorovich.ru>
2024-11-09 15:05:29 +0300

    misc router cli.py and some make commands

--- app/config.py
+++ app/config.py
@@ -8,10 +8,11 @@ class DBConfig(BaseModel):
     db_password: str
     db_host: str
     db_port: str
-    
+
     @property
     def db_url(self):
-        return f'postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}'
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+
 
 class APPConfig(BaseModel):
     app_port: int
@@ -25,10 +26,10 @@ class Settings(BaseModel):
     app: APPConfig
     db: DBConfig
 
-dyna_settings = Dynaconf(
-    settings_file=['settings.toml'],
 
+dyna_settings = Dynaconf(
+    settings_file=["settings.toml"],
 )
 
-settings = Settings(app=dyna_settings['app_settings'], db=dyna_settings['db_settings'])
+settings = Settings(app=dyna_settings["app_settings"], db=dyna_settings["db_settings"])
 print(settings.app.app_port)
---
+++ cli.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from typing import Annotated
+
+from typer import Typer, Argument
+from app.main import run_api_app
+
+cli = Typer(help="Pay service CLI")
+
+
+class Apps(str, Enum):
+    api = "api"
+    background_tasks = "background_tasks"
+
+@cli.command()
+def run_(
+    app: Annotated[Apps, Argument(help="App to run")] = Apps.api
+):
+    match app:
+        case Apps.api:
+            run_api_app()
+        
+        case Apps.background_tasks:
+            # TODO: make background tasks
+            pass
